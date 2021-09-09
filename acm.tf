0afe3453a5dce747995532faa17566e4fbc1acba
Leo Przybylski <leo@cloudposse.com>
2021-09-09 10:02:03 -0700

    fix(module): Updating version to AWS provider in the example and fixing alias naming to meet conventions

--- acm.tf
+++ acm.tf
@@ -1,4 +1,4 @@
-resource "aws_acm_certificate" "root" {
+resource "aws_acm_certificate" "default" {
   count             = local.acm_enabled ? 1 : 0
   private_key       = join("", tls_private_key.default.*.private_key_pem)
   certificate_body  = join("", tls_private_key.default.*.private_key_pem)
