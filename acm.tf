b9cff36a4f625b7fa0852472f662edce36acc665
franciscorosati <franrosatidev@gmail.com>
2023-11-24 10:07:43 -0300

    feat: Adding tags to the acm resource

--- acm.tf
+++ acm.tf
@@ -3,4 +3,5 @@ resource "aws_acm_certificate" "default" {
   private_key       = local.tls_key
   certificate_body  = local.tls_certificate
   certificate_chain = var.basic_constraints.ca ? null : var.certificate_chain.cert_pem
+  tags              = module.this.tags
 }
